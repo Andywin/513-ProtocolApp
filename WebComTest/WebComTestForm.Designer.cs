fec70dc3e9f8a63c5c6781ac9c2f162220551d51
Andy <winnerwbx@163.com>
2014-07-07 23:56:29 +0800

    添加网络通信测试

---
+++ WebComTest/WebComTestForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WebComTest
+{
+    partial class WebComTestForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPortNum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDataToSend = new System.Windows.Forms.TextBox();
+            this.textBoxIPtoSend = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxDataRcvd = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(6, 230);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "发送";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "接收";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "接收方IP地址：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxPortNum);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxDataToSend);
+            this.groupBox1.Controls.Add(this.buttonSend);
+            this.groupBox1.Controls.Add(this.textBoxIPtoSend);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(275, 264);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "发送数据";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "端口号：";
+            // 
+            // textBoxPortNum
+            // 
+            this.textBoxPortNum.Location = new System.Drawing.Point(102, 50);
+            this.textBoxPortNum.Name = "textBoxPortNum";
+            this.textBoxPortNum.Size = new System.Drawing.Size(73, 21);
+            this.textBoxPortNum.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "发送内容：";
+            // 
+            // textBoxDataToSend
+            // 
+            this.textBoxDataToSend.Location = new System.Drawing.Point(6, 102);
+            this.textBoxDataToSend.Multiline = true;
+            this.textBoxDataToSend.Name = "textBoxDataToSend";
+            this.textBoxDataToSend.Size = new System.Drawing.Size(260, 122);
+            this.textBoxDataToSend.TabIndex = 4;
+            // 
+            // textBoxIPtoSend
+            // 
+            this.textBoxIPtoSend.Location = new System.Drawing.Point(102, 23);
+            this.textBoxIPtoSend.Name = "textBoxIPtoSend";
+            this.textBoxIPtoSend.Size = new System.Drawing.Size(164, 21);
+            this.textBoxIPtoSend.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBoxDataRcvd);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(293, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(318, 278);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "接收数据";
+            // 
+            // textBoxDataRcvd
+            // 
+            this.textBoxDataRcvd.Location = new System.Drawing.Point(7, 25);
+            this.textBoxDataRcvd.Multiline = true;
+            this.textBoxDataRcvd.Name = "textBoxDataRcvd";
+            this.textBoxDataRcvd.Size = new System.Drawing.Size(305, 198);
+            this.textBoxDataRcvd.TabIndex = 2;
+            // 
+            // WebComTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 409);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "WebComTestForm";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDataToSend;
+        private System.Windows.Forms.TextBox textBoxIPtoSend;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxDataRcvd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPortNum;
+    }
+}
+
---
+++ WebComTest/WebComTestForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace WebComTest
+{
+    public partial class WebComTestForm : Form
+    {
+        //声明发送用UdpClient
+        UdpClient udpClientSend = null;
+        //声明接受用UdpClient
+        UdpClient udpClientRcv = null;
+
+        public WebComTestForm()
+        {
+            //屏蔽跨线程改控件属性那个异常
+            CheckForIllegalCrossThreadCalls = false;
+            //初始化
+            udpClientSend = new UdpClient();
+            udpClientRcv = new UdpClient(888);
+            InitializeComponent();
+
+            //开一线程
+            Thread th = new Thread(new ThreadStart(listen));
+            //设置为后台
+            th.IsBackground = true;
+            th.Start();
+        }
+
+        private void listen()
+        {
+           //声明终结点
+            IPEndPoint iep = null;
+            while (true)
+            {
+                //获得Form1发送过来的数据包
+                string text = System.Text.Encoding.UTF8.GetString(udpClientRcv.Receive(ref iep));
+                //更新接收到的内容
+                this.textBoxDataRcvd.Text = text;
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            //保存要发送的文本
+            string tempToSend = textBoxDataToSend.Text;
+            //将文本转化为字节数组
+            byte[] dataToSend = Encoding.ASCII.GetBytes(tempToSend);
+            //向指定IP地址发送数据
+            IPAddress addressTosend;
+            //int portToSend = Int32.Parse(textBoxPortNum.Text);
+            if(IPAddress.TryParse(textBoxIPtoSend.Text,out addressTosend))
+            {
+                //端口默认为888
+                udpClientSend.Connect(addressTosend, 888);
+                udpClientSend.Send(dataToSend, dataToSend.Length);
+            }
+        }
+
+
+
+    }
+}
